205aa9a25bcf1c93da56a10a0eda9793cad28c83
BridgeNY <zyq123258789@gmail.com>
2015-09-25 22:46:15 -0400

    Done with the quick union

--- Project1.java
+++ Project1.java
@@ -9,14 +9,14 @@ public class Project1 {
 	private int[][] grid;
 	private ArrayList<Point> connections;
 
-	// add an int array to store input location to map
-	private int[] intMap;
+	private static Scanner input = new Scanner(System.in);
+
 	// add an int arraylist to install pairs
-	private ArrayList<Integer> intPairs;
+	private ArrayList<Integer> intPairs = new ArrayList<Integer> ();
 	// add integer to indicate number of connections
 	private int num_connect = 0;
 	// add integer array to mark parent
-	private int[] parent = null;
+	private int[] parent;
 
 
 	/**
@@ -29,45 +29,35 @@ public class Project1 {
  		this.m = m;
 		this.n = n;
 		grid = new int[m][n];
-		intMap = new int[m * n];
-		// initiate intMap
-		for (int i = 0; i < m * n; i++) 
-			intMap[i] = i;
-
 	}
 
 	/**
 	 * Reads input from user (pair of connections presented as points), store the input in a list  
 	 */
 	public void read_input() {
-		String inputLine;
-		String[] twoLocation;
+		// input number of connections
 
-		// StdIn si = new StdIn(System.in);
-		System.out.print("Enter number of pairs of connections: ");
-		num_connect = StdIn.readInt();
-		System.out.println(" read int successfully " + num_connect);
-
-		// initiate intPairs
-		//intPairs = new int[num_connect * 2];
-
-		//for (int i = 0; i < num_connect; i++) {
-		while (!StdIn.isEmpty()) {	
-			// Read next line
-			inputLine = StdIn.readLine();
-			twoLocation = inputLine.split(" ");
-			// int x1 = Integer.parseInt(twoLocation[0]);
-			// int y1 = Integer.parseInt(twoLocation[1]);
-			// int x2 = Integer.parseInt(twoLocation[2]);
-			// int y2 = Integer.parseInt(twoLocation[3]);
-			// Point p1 = new Point(x1, y1);
-			// int intP1 = map(p1);
-			// intPairs.add(intP1);
-			// Point p2 = new Point(x2, y2);
-			// int intP2 = map(p2);
-			// intPairs.add(intP2);
-		}
+		num_connect = input.nextInt();
 		
+		// input conection pairs
+		for (int i = 0; i < num_connect; i++) {
+
+			int x1 = input.nextInt();
+			int y1 = input.nextInt();
+			int x2 = input.nextInt();
+			int y2 = input.nextInt();
+			// convert to points
+			Point p1 = new Point(x1, y1);
+			Point p2 = new Point(x2, y2);
+			// check if they are adjacent
+			if(!is_adjacent(p1, p2)) 
+				continue;
+			// add to intPairs if they are adjacent
+			int intP1 = map(p1);
+			intPairs.add(intP1);
+			int intP2 = map(p2);
+			intPairs.add(intP2);
+		}
 	}
 
 	/**
@@ -77,13 +67,14 @@ public class Project1 {
 	 */
 
 	public int map(Point p) {
+		// initiate mapvalue
 		int mapValue = 0;
+		// obtain x, y coordinates value
 		int x = (int)p.getX();
 		int y = (int)p.getY();
-		
-		mapValue = x * m + y + 1;
-		return mapValue;
-		
+		// formula to convert two dimensional to one dimensional value
+		mapValue = x * n + y;
+		return mapValue;	
 	}
 
 	/***
@@ -92,19 +83,13 @@ public class Project1 {
 	 * @return
 	 */
 	public  Point unmap(int i) {
+		// initiate the x, y coordiates value
 		int x = 0;
 		int y = 0;
 		Point p = null;	
-	
-		if ( i % m == 0 ) {
-			// edge of the matrix
-			x = i / m;
-			y = n - 1;
-		} else {
-			x = i / m;
-			y = i % m;
-			y -= 1;
-		}
+		// find x and y value
+		x = i / n;
+		y = i % n;
 		// initilize P value
 		p = new Point(x, y);
 		return p;
@@ -117,22 +102,27 @@ public class Project1 {
 		// define parent count
 		int parent_count = 0;
 		// initiate weightedquickunion
-		qu = new WeightedQuickUnionUF(m*n);
+		qu = new WeightedQuickUnionUF(m * n);
 		// weighted quick union
+		// method directed from weighted quick union main class
 		int size_arraylistConnect = intPairs.size();
 		for (int i = 0; i < size_arraylistConnect - 1; i = i + 2) {
 			int p = intPairs.get(i);
 			int q = intPairs.get(i + 1);
 			if (qu.connected(p, q)) continue;
             qu.union(p, q);
-            StdOut.println(p + " " + q);
 		}
+		// get the parent int array after union
 		parent = qu.getParent();
-
+		// renew parent array
+		for (int i = 0; i < parent.length; i++) {
+			parent[i] = qu.find(i);
+		}
+		
 		// Put parent array into grid
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				grid[m][n] = parent[parent_count];
+		for (int row = 0; row < m; row++) {
+			for (int col = 0; col < n; col++) {
+				grid[row][col] = parent[parent_count];
 				parent_count++;
 			}
 		}
@@ -146,20 +136,14 @@ public class Project1 {
 	public ArrayList<Point> retrieve_connected_sets() {
 		// initiate point arraylist
 		ArrayList<Point> listp = new ArrayList<Point>();
-		// define first parent point
-		// ************** do I need always check null? ************
-		int tem = 0;
+
 		// transfer integer array to arraylist
 		for (int i = 0; i < parent.length; i++) {
 			// traversal parent array and put its value to point array list
-			if (parent[i] != tem) {
-				Point temp = unmap(parent[i]);
-				listp.add(temp);
-				// renew tem value
-				tem = parent[i];
+			if (!listp.contains(unmap(parent[i]))) {
+				listp.add(unmap(parent[i]));
 			}
 		}
-
 		return listp;
 	}
 
@@ -175,7 +159,7 @@ public class Project1 {
 		int p1y = (int)p1.getY();
 		int p2x = (int)p2.getX();
 		int p2y = (int)p2.getY();
-
+		// 
 		if (p1x == p2x) {
 			if ( p1y - p2y == 1 || p1y - p2y == -1 )
 				result = true;
@@ -194,22 +178,22 @@ public class Project1 {
 		// int[0] Xmin; int[1] Xmax; int[2] Ymin; int[3] Ymax
 		int[] boundaryValue = new int[4];
 		
-		int Xmin = 0;
+		int Xmin = m;
 		int Xmax = 0;
-		int Ymin = 0;
+		int Ymin = n;
 		int Ymax = 0;
 
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < n; j++) {
-				if (grid[m][n] == candidate) {
-					if (m < Xmin)
-						Xmin = m;
-					if (m > Xmax)
-						Xmax = m;
-					if (n < Ymin)
-						Ymin = n;
-					if (n > Ymax)
-						Ymax = n; 
+				if (grid[i][j] == candidate) {
+					if (i < Ymin)
+						Ymin = i;
+					if (i > Ymax)
+						Ymax = i;
+					if (j < Xmin)
+						Xmin = j;
+					if (j > Xmax)
+						Xmax = j; 
 				}
 			}
 		}
@@ -252,14 +236,14 @@ public class Project1 {
 		}
 
 		// output the result
-		System.out.println("number of sets: " + parentSize);
+		// System.out.println("number of sets: " + parentSize);
 		// print second part of the result
 		for (int i = 0; i < parentSize; i++) {
 			//Point parent = unmap(sets[i])
 			int xValue = (int)sets.get(i).getX();
 			int yValue = (int)sets.get(i).getY();
 			int outSetSize = setofSetSize.get(i);
-			System.out.println("Parent (" + xValue + "," + yValue + ") with size " + parentSize);
+			System.out.println("Set (" + xValue + "," + yValue + ") with size " + outSetSize);
 		}
 		// print third part of the result
 		for (int j = 0; j < parentSize; j++) {
@@ -269,8 +253,8 @@ public class Project1 {
 			int intParent = map(sets.get(j));
 			// implement find boundaries
 			int[] boundariesValue = findBundaries(intParent);
-			System.out.println("Bounds for parent (" + xValue + "," + yValue + "): " + boundariesValue[0]
-			 + "<=x<=" + boundariesValue[1] + " " + boundariesValue[2] + "<=y<=" + boundariesValue[3]); 
+			System.out.println("Boundaries for (" + xValue + "," + yValue + ") are " + boundariesValue[0]
+			 + "<=x<=" + boundariesValue[1] + " and " + boundariesValue[2] + "<=y<=" + boundariesValue[3]); 
 		}		
 	}
 
@@ -278,8 +262,8 @@ public class Project1 {
 
 	public static void main(String args[]) {
 		int m, n;
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter size of grid(m n): ");
+		// Scanner input = new Scanner(System.in);
+		// System.out.print("Enter size of grid(m n): ");
 		m = input.nextInt();
 		n = input.nextInt();
 
@@ -288,5 +272,6 @@ public class Project1 {
 		project1.process_connections();
 		ArrayList<Point> sets = project1.retrieve_connected_sets();
 		project1.output_boundaries_size(sets);
+		input.close();
 	}
 }
